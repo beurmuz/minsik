9fac8fca38a0f223f8b76317380f3d487142eb72
beurmuz <fallinta2@gmail.com>
2023-04-25 18:09:07 +0900

    #5 [dev] GotoUpButton 개발

---
+++ src/components/GotoTopButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FiArrowUpCircle } from "react-icons/fi";
+
+export const GotoTopButton = (props) => {
+  return (
+    <div>
+      <button>
+        <FiArrowUpCircle size='40' />
+      </button>
+    </div>
+  );
+};
