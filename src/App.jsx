c5a8dfd499477ed633aca64a9ece7bad80e4639a
beurmuz <fallinta2@gmail.com>
2023-04-25 17:54:26 +0900

    #3 [dev] Footer 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Menu } from "./components/Menu";
 
 function App() {
   return (
     <div className='App'>
-      <Menu />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = (props) => {
+  return (
+    <div class='flex flex-col justify-center text-center'>
+      <p class=' text-gray-300 font-Pretendard text-xs'>
+        Contact | fallinta2@gmail.com
+      </p>
+      <p class=' text-gray-300 font-Pretendard text-xs'>
+        Copyright | ⓒ 2023 beurmuz. All rights reserved.
+      </p>
+    </div>
+  );
+};
