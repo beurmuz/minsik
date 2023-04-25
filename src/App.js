66284de7b535d6434cb2cd5edc9b92d696d1c028
beurmuz <fallinta2@gmail.com>
2023-04-25 19:37:14 +0900

    #6 [dev] Toolbar 만들기

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Toolbar } from "./components/Toolbar";
+import { Menu } from "./components/Menu";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Toolbar />
+      {/* <Menu /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Toolbar = (props) => {
+  return (
+    <div class='font-Pretendard text-white bg-black py-1.5'>
+      @younghotyellow94 💿 @younghotyellow94 📀 @younghotyellow94 💿
+      @younghotyellow94
+    </div>
+  );
+};
