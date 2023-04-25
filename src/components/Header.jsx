1275aa7201a8d597eae6c5cccf7e6fffb2dcd300
beurmuz <fallinta2@gmail.com>
2023-04-25 16:14:45 +0900

    #2 [dev] react-icons 추가하기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { IoIosMenu } from "react-icons/io";
 
 export const Header = (props) => {
   return (
     <div class='flex flex-row justify-between p-5'>
       <h1 class='font-Pretendard text-3xl font-bold '>SIK-K</h1>
-      <nav class='font-Pretendard text-3xl font-bold'>MENU</nav>
+      <IoIosMenu size='40' />
     </div>
   );
 };
