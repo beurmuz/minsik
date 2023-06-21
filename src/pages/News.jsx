deb967facbb03209583d3045d4d24313694dbbd2
beurmuz <fallinta2@gmail.com>
2023-06-22 00:23:53 +0900

    #17 [dev] news 크롤링 결과 화면에 그리기

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { NewsList } from "../components/NewsList";
-import { Post } from "../components/Post";
+// import { Post } from "../components/Post";
+import newsData from "../crawlingData/news_data.json";
 
 export const News = (props) => {
+  const newsList = newsData.map((article) => {
+    return (
+      <li
+        class='bg-sky-50/80  font-NotoKo border rounded-md p-4 my-3 text-base list-none'
+        key={article.id}
+      >
+        <h3 class=' font-Pretendard font-bold text-lg pb-2 leading-tight'>
+          {article.title}
+        </h3>
+        <p class='font-Pretendard text-sm'>
+          {article.content.slice(0, 101) + "..."}
+        </p>
+        <p class='text-right pt-2'>
+          <span class='font-Pretendard text-sm font-bold'>{article.date}</span>
+        </p>
+      </li>
+    );
+  });
+
   return (
     <div class='w-full h-full flex flex-col '>
       <Header />
       <div class='m-7'>
         <article>
-          <h1 class='text-white'>News</h1>
-          <ol class='my-10'>
-            <NewsList>웅 뉴스얌</NewsList>
-            <NewsList>웅 이것이 뉴스</NewsList>
-            <NewsList>웅 뉴스얌</NewsList>
-            <NewsList>웅 이것이 뉴스</NewsList>
-            <NewsList>웅 뉴스얌</NewsList>
-            <NewsList>웅 이것이 뉴스</NewsList>
-          </ol>
+          <p class='text-white font-NotoSerif text-sm'>
+            Last Update: 2023.06.21 20:00 PM
+          </p>
+          <ol class='my-5 mb-10'>{newsList}</ol>
         </article>
-        <article>
+        {/* <article>
           <h1 class='text-white'>Instagram</h1>
           <div class='my-10 grid grid-cols-3'>
             <Post />
@@ -32,7 +46,7 @@ export const News = (props) => {
             <Post />
             <Post />
           </div>
-        </article>
+        </article> */}
       </div>
     </div>
   );
