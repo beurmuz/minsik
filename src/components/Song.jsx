05b385cc66429e48cb4966b8809bf9b9602fdc1e
beurmuz <fallinta2@gmail.com>
2023-06-24 18:48:03 +0900

    #16 [style] font 변경

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -4,13 +4,13 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 
 export const Song = ({ title, artists }) => {
   return (
-    <li class='text-white font-NotoKo border-b p-3 text-base list-none flex felx-row'>
+    <li class='text-white border-b p-3 text-base list-none flex felx-row'>
       <div class='w-10'>
         <img src={basicImg} alt='album' class='' />
       </div>
       <div class=' flex flex-col justify-between m-auto ml-3'>
-        <h2 class='font-semibold text-base'>{title}</h2>
-        <h3 class='font-normal text-xs'>{artists}</h3>
+        <h2 class='font-normal text-base font-Pretendard'>{title}</h2>
+        <h3 class=' text-xs font-Pretendard'>{artists}</h3>
       </div>
       <div class='mx-1 my-auto'>
         <BsThreeDotsVertical size='20' />
