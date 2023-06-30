df644121671f52a2208303a7a1db7316f3a9bb3a
beurmuz <fallinta2@gmail.com>
2023-06-30 18:18:38 +0900

    #24 [dev] songs crawler 개발

--- src/crawler/songs_crawler.py
+++ src/crawler/songs_crawler.py
@@ -6,22 +6,44 @@ import json
 
 # ConnectionError 방지용
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Whale/3.21.192.18"}
-
-
-url = "https://www.melon.com/artist/song.htm?artistId=786648#params%5BlistType%5D=A&params%5BorderBy%5D=ISSUE_DATE&params%5BartistId%5D=786648&po=pageObj&startIndex=1"
-songs = requests.get(url, headers=headers)
-songsHtml = BeautifulSoup(songs.text,"html.parser")
-totalSongs = songsHtml.select('table > tbody > tr')
+pageNums = [1, 51, 101, 151]
 songsData = []
+songsId = []
 
-for i in range(len(totalSongs)):
-  m = totalSongs[i]
-  songsData.append({
-      "No": m.select_one('td.no > div.wrap').text,
-      "title": m.select_one('a.fc_gray').text,
-      "artists": m.select_one('span.checkEllipsis').text,
-      "ablum": m.select('div.ellipsis')[2].text.strip()
-  })
+for j in pageNums:
+  url = "https://www.melon.com/artist/songPaging.htm?startIndex="+str(j)+"&pageSize=50&listType=A&orderBy=ISSUE_DATE&artistId=786648"
+  songs = requests.get(url, headers=headers)
+  songsHtml = BeautifulSoup(songs.text,"html.parser")
+  totalSongs = songsHtml.select('table > tbody > tr')
+
+  for i in range(len(totalSongs)):
+    m = totalSongs[i]
+    try:
+      songsId.append(m.select_one('input.input_check')['value'])
+      songsData.append({
+        "no": m.select_one('td.no > div.wrap').text,
+        "songId": m.select_one('input.input_check')['value'],
+        "title": m.select_one('a.fc_gray').text,
+        "artists": m.select_one('span.checkEllipsis').text,
+        "ablum": m.select('div.ellipsis')[2].text.strip()
+      })
+    except Exception as e:
+      continue
+
+# 곡 상세정보 가져오기
+songsInfo = []
+
+for i in range(0, len(songsData)):
+  url = "https://www.melon.com/song/detail.htm?songId="+str(songsId[i])
+  song = requests.get(url, headers=headers)
+  songHtml = BeautifulSoup(song.text, "html.parser")
+  info = songHtml.find('div', attrs={'id':'conts'})
+  meta = info.find('dl', attrs={'class':'list'})
+  release = meta.select('dd')[1].text
+  imgSource = info.find('img', attrs={'width': '282'})['src']
+  
+  songsData[i]["release"] = release
+  songsData[i]["imgSource"] = imgSource
 
 # json 형식으로 만들기
 with open('src/crawlingData/songs_data.json', "w", encoding="UTF-8-sig") as f_write:
