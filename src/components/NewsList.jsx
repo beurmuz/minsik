c9fbe4cc9b62e158e500796006cf3b65de220e0e
beurmuz <fallinta2@gmail.com>
2023-08-01 19:05:31 +0900

    #22 [fix] news Data 가져오기 문제 해결

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { newsApi } from "../shared/axios";
+import newsData from "../crawlingData/news_data.json";
 import { NewsItem } from "./NewsItem";
 
 export const NewsList = () => {
-  const [newsData, setNewsData] = useState([]);
+  // const [newsData, setNewsData] = useState([]);
 
-  const getNewsData = async () => {
-    const result = await fetch(
-      "https://raw.githubusercontent.com/beurmuz/minsik/main/src/crawlingData/news_data.json"
-    )
-      .then((res) => res.json())
-      .catch("Data Load Error");
-    setNewsData(result);
-  };
+  // const getNewsData = async () => {
+  //   const result = await newsApi.get("news_data.json");
+  //   // setNewsData(result);
+  //   console.log(result);
+  //   // const result = await fetch(
+  //   //   "https://raw.githubusercontent.com/beurmuz/minsik/main/src/crawlingData/news_data.json"
+  //   // )
+  //   //   .then((res) => res.json())
+  //   //   .catch("Data Load Error");
+  //   // setNewsData(result);
+  // };
 
-  useEffect(() => {
-    getNewsData();
-  }, []);
+  // useEffect(() => {
+  //   getNewsData();
+  // }, []);
 
   return (
     <ol class='my-5 mb-10'>
--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,23 +1,19 @@
 import axios from "axios";
-// const BASE_URL = "https://github.com/beurmuz/minsik/blob/main/src/crawlingData/";
+const BASE_URL =
+  "https://raw.githubusercontent.com/beurmuz/minsik/main/src/crawlingData/";
 
 export const songsApi = axios.create({
-  baseURL:
-    "https://github.com/beurmuz/minsik/blob/main/src/crawlingData/songs_data.json",
+  baseURL: BASE_URL,
   responseType: "json",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
-
 export const newsApi = axios.create({
-  baseURL: "https://github.com/beurmuz/minsik/blob/main/src/crawlingData/",
+  baseURL: BASE_URL,
   responseType: "json",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": `http://localhost:3000`,
-    "Access-Control-Allow-Credentials": "true",
   },
 });
