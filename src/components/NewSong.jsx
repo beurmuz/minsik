c2fb735ac8df49294b4c488e91f6869e8978eab9
beurmuz <fallinta2@gmail.com>
2023-08-27 20:48:03 +0900

    #16 [dev] 최신 앨범정보  추가하기

---
+++ src/components/NewSong.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const NewSong = ({ songInfo }) => {
+  const { imgSource, ablum, artists, release } = { ...songInfo };
+  return (
+    <div className="flex flex-row px-3 pt-3 pb-10">
+      <div className="w-1/2 min-[700px]:w-1/4">
+        <img src={imgSource} className="w-full h-auto" />
+      </div>
+      <article className="flex flex-col justify-center w-1/2 pl-5 min-[700px]:w-3/4">
+        <h3 class="font-Pretendard text-sm text-main-blue/80 font-bold pb-3 max-[540px]:pb-1">
+          최신 앨범
+        </h3>
+        <p className="font-Pretendard text-4xl font-bold text-main-blue pb-2 max-[540px]:text-2xl">
+          {ablum}
+        </p>
+        <p className="font-Pretendard text-xl max-[540px]:text-base">
+          {artists}
+        </p>
+        <p className="font-Pretendard">{release}</p>
+      </article>
+    </div>
+  );
+};
--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -3,6 +3,7 @@ import { SongItem } from "./SongItem";
 import { songsApi } from "../shared/axios";
 // import { RaceBy } from "@uiball/loaders";
 import { dataStore } from "../shared/store";
+import { NewSong } from "./NewSong";
 
 export const SongsList = (props) => {
   const { releaseSongs, setsReleaseSongs, joinSongs, setsJoinSongs } =
@@ -39,52 +40,62 @@ export const SongsList = (props) => {
   }, []);
 
   return (
-    <div class="m-7 p-30 overflow-auto">
-      <div class="w-full mb-3 flex flex-row justify-end">
-        <button
-          class="mx-3 font-NotoKo text-gray-500 hover:text-main-blue"
-          onClick={(e) => setOrderState("release")}
-        >
-          발매 ({releaseSongs.length})
-        </button>
-        <span class="border-r border-gray-500 h-4 my-auto"></span>
-        <button
-          class="mx-3 font-NotoKo text-gray-500 hover:text-main-blue"
-          onClick={() => setOrderState("join")}
-        >
-          참여 ({joinSongs.length})
-        </button>
+    <div className="m-7 p-30 ">
+      <NewSong songInfo={releaseSongs[0]} />
+
+      {/* 전체곡 리스트 */}
+      <div className="overflow-auto">
+        <div className="w-full mb-3 flex flex-row justify-between">
+          <p className="font-Pretendard text-sm text-main-blue/80 text-xl font-bold pl-3">
+            전체곡
+          </p>
+          <div className="">
+            <button
+              className="mx-3 font-Pretendard text-gray-500 hover:text-main-blue"
+              onClick={(e) => setOrderState("release")}
+            >
+              발매 ({releaseSongs.length})
+            </button>
+            <span className="border-r border-gray-500 h-4 my-auto"></span>
+            <button
+              className="mx-3 font-Pretendard text-gray-500 hover:text-main-blue"
+              onClick={() => setOrderState("join")}
+            >
+              참여 ({joinSongs.length})
+            </button>
+          </div>
+        </div>
+        <ol class="">
+          {
+            releaseSongs && orderState === "release"
+              ? releaseSongs.map((song) => {
+                  return (
+                    <SongItem
+                      key={song.songId}
+                      title={song.title}
+                      artists={song.artists}
+                      imgSrc={song.imgSource}
+                    />
+                  );
+                })
+              : joinSongs.map((song) => {
+                  return (
+                    <SongItem
+                      key={song.songId}
+                      title={song.title}
+                      artists={song.artists}
+                      imgSrc={song.imgSource}
+                    />
+                  );
+                })
+            // <div class="flex h-full">
+            //   <span class="m-auto">
+            //     <RaceBy size={50} lineWeight={3} speed={1.4} color="gray" />
+            //   </span>
+            // </div>
+          }
+        </ol>
       </div>
-      <ol class="">
-        {
-          releaseSongs && orderState === "release"
-            ? releaseSongs.map((song) => {
-                return (
-                  <SongItem
-                    key={song.songId}
-                    title={song.title}
-                    artists={song.artists}
-                    imgSrc={song.imgSource}
-                  />
-                );
-              })
-            : joinSongs.map((song) => {
-                return (
-                  <SongItem
-                    key={song.songId}
-                    title={song.title}
-                    artists={song.artists}
-                    imgSrc={song.imgSource}
-                  />
-                );
-              })
-          // <div class="flex h-full">
-          //   <span class="m-auto">
-          //     <RaceBy size={50} lineWeight={3} speed={1.4} color="gray" />
-          //   </span>
-          // </div>
-        }
-      </ol>
     </div>
   );
 };
