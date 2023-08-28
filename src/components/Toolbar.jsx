4e1cc30a11350f1045b88387879045587c634ec6
beurmuz <fallinta2@gmail.com>
2023-08-29 01:21:47 +0900

    #6 [fix] toolbar 수정 및 애니메이션 제작, 추가

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export const Toolbar = (props) => {
   return (
-    <div className="font-Pretendard text-white bg-black py-1.5">
-      @younghotyellow94 @younghotyellow94 @younghotyellow94
+    <div className="bg-black flex overflow-hidden">
+      <div className="font-Pretendard w-screen h-9 flex flex-nowrap m-auto p-1 whitespace-nowrap text-white bg-black animate-marqueeOrigin max-[540px]:animate-marqueeResponsive">
+        @younghotyellow94 @younghotyellow94 @younghotyellow94 @younghotyellow94
+        @younghotyellow94 @younghotyellow94 @younghotyellow94 @younghotyellow94
+        @younghotyellow94 @younghotyellow94 @younghotyellow94 @younghotyellow94
+        @younghotyellow94 @younghotyellow94 @younghotyellow94 @younghotyellow94
+      </div>
     </div>
   );
 };
