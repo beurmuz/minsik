47ebddc773149d52019cfde3ab2fa57bedb1af9c
beurmuz <fallinta2@gmail.com>
2023-08-28 18:19:05 +0900

    #22 [fix] 스크래핑 수 줄이기

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { newsApi } from "../shared/axios";
 import { NewsItem } from "./NewsItem";
-import { RaceBy } from "@uiball/loaders";
 
 export const NewsList = () => {
   const [newsData, setNewsData] = useState([]);
@@ -20,29 +19,20 @@ export const NewsList = () => {
 
   return (
     <ol className="my-5 mb-10">
-      {newsData ? (
+      {newsData &&
         newsData.map((data) => {
-          if (data.id < 5) {
-            return (
-              <NewsItem
-                id={data.id}
-                key={data.id}
-                title={data.title}
-                link={data.link}
-                content={data.content}
-                media={data.media}
-                date={data.date}
-              />
-            );
-          }
-        })
-      ) : (
-        <div className="flex h-full">
-          <span className="m-auto">
-            <RaceBy size={50} lineWeight={3} speed={1.4} color="gray" />
-          </span>
-        </div>
-      )}
+          return (
+            <NewsItem
+              id={data.id}
+              key={data.id}
+              title={data.title}
+              link={data.link}
+              content={data.content}
+              media={data.media}
+              date={data.date}
+            />
+          );
+        })}
     </ol>
   );
 };
