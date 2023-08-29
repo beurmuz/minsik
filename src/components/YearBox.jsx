faecf7a7b08f67203b6f714b7a432c1f206f7091
beurmuz <fallinta2@gmail.com>
2023-08-29 21:18:08 +0900

    #10 [fix] 연도 앞 스타일 움직이는걸로 변경

--- src/components/YearBox.jsx
+++ src/components/YearBox.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { TbPointFilled } from "react-icons/tb";
 
 export const YearBox = ({ children }) => {
   return (
-    <div className="flex flex-row mx-9">
-      <div className="w-5 h-5 border-8 border-main-blue-light rounded-full mx-0 my-auto"></div>
-      <h2 className="font-Pretendard text-2xl font-bold text-main-blue-light px-2">
+    <div className="flex flex-row mx-6">
+      {/* <div className="w-5 h-5 border-8 border-main-blue-light rounded-full mx-0 my-auto" />*/}
+      <div className="mx-2">
+        <span className="m-auto animate-ping text-main-blue-light">
+          <TbPointFilled size="30" />
+        </span>
+      </div>
+      <h2 className="font-Pretendard text-3xl font-bold text-main-blue-light px-1">
         {children}
       </h2>
     </div>
