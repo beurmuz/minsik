78f8b3d0e5cf0533f130f6c1130822076fd59174
beurmuz <fallinta2@gmail.com>
2023-08-31 17:25:06 +0900

    #29 [dev] useModal Hooks 개발

---
+++ src/Hooks/useModal.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const useModal = (props) => {
+  const [showModal, setShowModal] = useState(false);
+
+  function toggle() {
+    setShowModal(!showModal);
+  }
+
+  return {
+    showModal,
+    toggle,
+  };
+};
+
+export default useModal;
