a1231d743b410c1882a602852d039bdc17536e2b
beurmuz <fallinta2@gmail.com>
2023-09-06 13:35:05 +0900

    #33 [dev] og 태그 수정

--- src/SEO/MetadataTemplate.jsx
+++ src/SEO/MetadataTemplate.jsx
@@ -12,16 +12,9 @@ const MetadataTemplate = ({
     <Helmet>
       <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
-
-      <meta property="og:type" content="website" />
       <meta property="og:url" content={ogUrl} />
       <meta property="og:title" content={ogTitle} />
       <meta property="og:description" content={ogDescription} />
-      <meta property="og:site_name" content="SIK-K" />
-      <meta property="og:locale" content="ko_KR" />
-      <meta property="og:image" content="%PUBLIC_URL%/metaImg.png" />
-
-      <link rel="canonical" href="" />
     </Helmet>
   );
 };
