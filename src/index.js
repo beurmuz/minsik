cec179cacc7870409966f8ec8fa5ac7d9e2caba7
beurmuz <fallinta2@gmail.com>
2023-09-06 14:59:24 +0900

    #33 [dev] react-snap 적용

--- src/index.js
+++ src/index.js
@@ -5,14 +5,36 @@ import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <HelmetProvider>
+//       <App />
+//     </HelmetProvider>
+//   </React.StrictMode>
+// );
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
