1eb9a1f6a44936c60754c6939f1a7db048ba25c8
beurmuz <fallinta2@gmail.com>
2026-09-09 15:06:03 +0900

    fix: 코드 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
 import Footer from "../components/Footer";
 import homeLogo from "../assets/images/homeLogo.svg";
 import MetadataTemplate from "../SEO/MetadataTemplate";
@@ -17,24 +16,24 @@ const Home = () => {
         ogTitle="SIK-K 사이트"
         ogDescription="8년차 팬이 만든 SIK-K(식케이) 웹 사이트"
       />
-      
-      <Helmet>
-        <link rel="preload" as="image" href={imageURL} fetchPriority="high" />
-      </Helmet>
 
-      <div
-        className="w-full h-screen flex flex-col justify-between bg-cover bg-center overflow-hidden"
-        style={{
-          backgroundImage: `url(${imageURL})`,
-        }}
-      >
+      <div className="relative w-full h-screen flex flex-col justify-between overflow-hidden">
+        <img
+          src={imageURL}
+          alt=""
+          aria-hidden="true"
+          fetchPriority="high"
+          className="absolute inset-0 w-full h-full object-cover object-center -z-10"
+        />
+
         <div className="h-[120px] shrink-0" aria-hidden="true" />
 
         <section
-          className={`${SITE_CONTAINER} flex-1 flex flex-col justify-center text-center animate-pageLoadEffect`}
+          className={`${SITE_CONTAINER} flex-1 flex flex-col justify-center text-center`}
         >
           <div className="flex flex-col gap-20">
             <img
+              fetchPriority="high"
               src={homeLogo}
               className="m-auto w-72 animate-[spin_10s_linear_infinite]"
               alt="SIK-K 메인 로고"
